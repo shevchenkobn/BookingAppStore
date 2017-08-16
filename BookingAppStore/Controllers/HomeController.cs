2511e32b849f673a70b11bbb1ec1d89cf7c3fa55
shevchenkobn <bohdan.shevchenko1@nure.ua>
2017-08-16 16:47:35 +0300

    commit1

--- BookingAppStore/Controllers/HomeController.cs
+++ BookingAppStore/Controllers/HomeController.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BookingAppStore.Models;
+using System.Diagnostics;
 
 namespace BookingAppStore.Controllers
 {
     public class HomeController : Controller
     {
+        BookContext db = new BookContext();
         public ActionResult Index()
         {
+            var books = db.Books;
+            
+            ViewBag.Books = books;
             return View();
         }
 
