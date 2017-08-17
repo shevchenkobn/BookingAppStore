9f0dd719c0e3ca1bc4112c0bc0c9af4190e98c5e
shevchenkobn <bohdan.shevchenko1@nure.ua>
2017-08-17 12:54:46 +0300

    module 9

--- ControllersBasics/Controllers/HomeController.cs
+++ ControllersBasics/Controllers/HomeController.cs
@@ -26,5 +26,30 @@ namespace ControllersBasics.Controllers
 
             return View();
         }
+
+        public string Square(int a = 3, int h = 10)
+        {
+            return $"Square of triangle (a = {a} and h = {h} is {a * h / 2}";
+        }
+        public string Square()
+        {
+            int.TryParse(Request.Params["a"], out int a);
+            int.TryParse(Request.Params["h"], out int h);
+            return Square(a, h);
+        }
+
+        [HttpGet]
+        public ActionResult GetBook()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string PostBook()
+        {
+            string result = "1st Way:<br>" + Request.Form["name"] + ", " + Request.Form["address"] + "<br>" +
+                "2nd Way:<br>" + Request.Form[0] + ", " + Request.Form[1];
+            return result;
+        }
     }
 }
