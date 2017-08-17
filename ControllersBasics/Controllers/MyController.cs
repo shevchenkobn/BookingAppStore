f0861e1da9b11430e5120ca8d0bbefb2663931e3
shevchenkobn <bohdan.shevchenko1@nure.ua>
2017-08-17 12:29:22 +0300

    module 8

---
+++ ControllersBasics/Controllers/MyController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ControllersBasics.Controllers
+{
+    public class MyController : IController
+    {
+        public void Execute(RequestContext requestContext)
+        {
+            string ip = requestContext.HttpContext.Request.UserHostAddress;
+            var respond = requestContext.HttpContext.Response;
+            respond.Write("Your IP: " + ip);
+        }
+    }
+}
---
+++ ControllersBasics/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ControllersBasics.MvcApplication" Language="C#" %>
