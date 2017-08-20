f675e102aef9f59b4e208bab1f6b3445ac074da5
shevchenkobn <bohdan.shevchenko1@nure.ua>
2017-08-21 00:43:23 +0300

    module 15

---
+++ AsyncTaskApp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using AsyncTaskApp.Models;
+
+namespace AsyncTaskApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        BookContext context = new BookContext();
+        public ActionResult Index()
+        {
+            var books = context.Books.ToList();
+            ViewBag.books = books;
+            return View();
+        }
+
+        public async Task<ActionResult> BookList()
+        {
+            var books = await context.Books.ToListAsync();
+            ViewBag.books = books;
+            return View("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ AsyncTaskApp/Models/BookContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AsyncTaskApp.Models;
+using System.Data.Entity;
+
+namespace AsyncTaskApp.Models
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+
+    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext context)
+        {
+            context.Books.Add(new Book { Name = "Voyna i mir", Author = "L. Tolstoy", Price = 220 });
+            context.Books.Add(new Book { Name = "Otsy i dety", Author = "I. Turgenev", Price = 185 });
+            context.Books.Add(new Book { Name = "Chayka", Author = "A. Chehov", Price = 130 });
+
+            base.Seed(context);
+        }
+    }
+}
