93fcf34c7ad3fd8326eb6bef669ecd77d05aa841
shevchenkobn <bohdan.shevchenko1@nure.ua>
2017-08-24 11:31:22 +0300

    module 27

---
+++ NavigationApp/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NavigationApp.Models;
+using System.Data.Entity;
+using System.Net;
+
+namespace NavigationApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        SoccerContext db = new SoccerContext();
+        public ActionResult Index()
+        {
+            var players = db.Players.Include(p => p.Team);
+            return View(players.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult TeamDetails(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            var team = db.Teams.Include(t => t.Players).FirstOrDefault(t => t.Id == id);
+            if (team == null)
+                return HttpNotFound();
+            return View(team);
+        }
+
+        //[HttpGet]
+        //public ActionResult Create()
+        //{
+
+        //}
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ NavigationApp/Models/SoccerContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace NavigationApp.Models
+{
+    public class SoccerContext : DbContext
+    {
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Player> Players { get; set; }
+
+
+    }
+}
