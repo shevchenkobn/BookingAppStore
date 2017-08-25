4e40b808cc451916c3f4aeb942d5c00730dcebf2
shevchenkobn <bohdan.shevchenko1@nure.ua>
2017-08-25 19:56:47 +0300

    module 29

---
+++ ManyToManyApp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ManyToManyApp.Models;
+using System.Net;
+
+namespace ManyToManyApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        StudentContext db = new StudentContext();
+        public ActionResult Index()
+        {
+            return View(db.Students.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            var student = db.Students.Find(id);
+            if (student == null)
+                return HttpNotFound();
+            return View(student);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ManyToManyApp/Models/StudentContext.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ManyToManyApp.Models
+{
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        public StudentContext() : base("DefaultConnection")
+        {}
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().HasMany(c => c.Students)
+                .WithMany(s => s.Courses)
+                .Map(t => t.MapLeftKey("CourseId")
+                .MapRightKey("StudentId")
+                .ToTable("CourseStudent"));
+        }
+    }
+
+    public class StudentDbInitializer : DropCreateDatabaseAlways<StudentContext>
+    {
+        protected override void Seed(StudentContext context)
+        {
+            var s1 = new Student { Id = 1, Name = "Yegor", Surname = "Ivanov" };
+            var s2 = new Student { Id = 2, Name = "Maria", Surname = "Vasilyeva" };
+            var s3 = new Student { Id = 3, Name = "Oleg", Surname = "Kuznetsov" };
+            var s4 = new Student { Id = 4, Name = "Olga", Surname = "Petrova" };
+
+            context.Students.Add(s1);
+            context.Students.Add(s2);
+            context.Students.Add(s3);
+            context.Students.Add(s4);
+
+            var c1 = new Course
+            {
+                Id = 1,
+                Name = "Operational systems",
+                Students = new List<Student> { s1, s2, s3 }
+            };
+            var c2 = new Course
+            {
+                Id = 2,
+                Name = "Algorithm and data structures",
+                Students = new List<Student> { s2, s4 }
+            };
+            var c3 = new Course
+            {
+                Id = 3,
+                Name = "HTML and CSS basics",
+                Students = new List<Student> { s1, s4, s3 }
+            };
+
+            context.Courses.Add(c1);
+            context.Courses.Add(c2);
+            context.Courses.Add(c3);
+
+            context.SaveChanges();
+            base.Seed(context);
+        }
+    }
+}
