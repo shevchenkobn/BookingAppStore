3ac03c89758eeaca987567c2981fc0a2561cb2ed
shevchenkobn <bohdan.shevchenko1@nure.ua>
2017-08-25 21:37:19 +0300

    module 29

--- ManyToManyApp/Controllers/HomeController.cs
+++ ManyToManyApp/Controllers/HomeController.cs
@@ -26,6 +26,59 @@ namespace ManyToManyApp.Controllers
             return View(student);
         }
 
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            var student = db.Students.Find(id);
+            if (student == null)
+                return HttpNotFound();
+            ViewBag.Courses = db.Courses.ToList();
+            return View(student);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student studentData, int[] selectedCourses)
+        {
+            var student = db.Students.Find(studentData.Id);
+            student.Name = studentData.Name;
+            student.Surname = studentData.Surname;
+
+            student.Courses.Clear();
+            if (selectedCourses != null)
+            {
+                foreach (var course in db.Courses.Where(c => selectedCourses.Contains(c.Id)))
+                {
+                    student.Courses.Add(course);
+                }
+            }
+
+            db.Entry(student).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.Courses = db.Courses.ToList();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Student student, int[] selectedCourses)
+        {
+            student.Courses.Clear();
+            if (selectedCourses != null)
+            {
+                foreach (var course in db.Courses.Where(c => selectedCourses.Contains(c.Id)))
+                    student.Courses.Add(course);
+            }
+            db.Students.Add(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
